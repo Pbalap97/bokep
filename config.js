7ab919845b1c6d459f036b406f2646a1c71ad589
Pbalap97 <165455156+Pbalap97@users.noreply.github.com>
2026-03-28 05:36:38 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+global.owner = {
+ownername: "prastzy!",
+ownernumber: ["NOMOROWNER"],
+botname: "FANATIC V2 VIP",
+botnumber: "NOMORBOTMU", 
+version: "v2.0.0",
+session: "session",
+wm: "FANATIK - PrsTzyy"
+}
+//---===================================//
